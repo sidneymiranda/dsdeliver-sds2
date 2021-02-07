682f34fbf023f464af1ddf979b7c23af16e59dc6
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-02-07 05:49:38 -0300

    endpoint: GET /orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dsdeliver.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status;
+	
+	private List<ProductDTO> products = new ArrayList<>();
+
+	public OrderDTO(Order entity) {
+		id = entity.getId();
+		address = entity.getAddress();
+		latitude = entity.getLatitude();
+		longitude = entity.getLongitude();
+		moment = entity.getMoment();
+		status = entity.getStatus();
+		products = entity.getProducts().stream()
+										.map(p -> new ProductDTO(p))
+										.collect(Collectors.toList());
+	}
+}
