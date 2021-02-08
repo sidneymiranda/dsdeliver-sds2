2faacdbee8634dd12ee07d31991ec22fc202f83f
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-02-08 01:33:44 -0300

    Add Navbar

---
+++ front-web/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+import './styles.css';
+
+function Navbar() {
+  return (
+    <nav className="main-navbar">
+      <Logo />
+      <a href="home" className="logo-text">
+        DS Delivery
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
